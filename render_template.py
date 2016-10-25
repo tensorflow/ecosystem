abfff5233860b46847d76fea853f3291748e19f3
Jonathan Hseu <vomjom@vomjom.net>
2016-10-25 14:05:28 -0700

    Take input from a file argument in render_template.py. (#7)

--- render_template.py
+++ render_template.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 
+from __future__ import print_function
+
 import jinja2
 import sys
 
-print(jinja2.Template(sys.stdin.read()).render())
+if len(sys.argv) != 2:
+  print("usage: {} [template-file]".format(sys.argv[0]), file=sys.stderr)
+  sys.exit(1)
+with open(sys.argv[1], "r") as f:
+  print(jinja2.Template(f.read()).render())
