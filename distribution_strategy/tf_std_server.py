2fc66ad5b9a635fd25d67f10c4c0b1554493c9fb
yuefengz <yuefengz@google.com>
2018-09-06 12:12:00 -0700

    Add more comments to tf_std_server.py

--- distribution_strategy/tf_std_server.py
+++ distribution_strategy/tf_std_server.py
@@ -21,10 +21,13 @@ import tensorflow as tf
 
 
 def main(unused_argv):
+  # Contrib ops are lazily loaded. So we touch one contrib module to load them
+  # immediately.
   to_import_contrib_ops = tf.contrib.resampler
 
-  # Load you custom ops here.
+  # Load you custom ops here before starting the standard TensorFlow server.
 
+  # Start and join the standard TensorFlow server.
   tf.contrib.distribute.run_standard_tensorflow_server().join()
 
 
