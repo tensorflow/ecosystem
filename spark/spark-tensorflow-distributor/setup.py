8c0528eafbf0275dcf51ec23c8e6cd5e4ef4e410
sarthfrey-db <sarth.frey@databricks.com>
2020-04-23 17:15:18 -0700

    update version

--- spark/spark-tensorflow-distributor/setup.py
+++ spark/spark-tensorflow-distributor/setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
   name="spark_tensorflow_distributor",
-  version="0.0.1",
+  version="0.0.2",
   author="sarthfrey",
   author_email="sarth.frey@gmail.com",
   description="This package helps users do distributed training with TensorFlow on their Spark clusters.",
