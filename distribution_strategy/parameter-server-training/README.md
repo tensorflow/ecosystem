a94b1c66c61ce54f10f3329765e531b5cffb2995
Selcuk Gun <5055758+selcukgun@users.noreply.github.com>
2021-02-09 11:12:19 +0000

    Add parameter server train & side-car eval on k8s
    ResNet56 model (with custom training loop) variables are created on
    parameter server jobs, and updated by workers. Evaluation is done using
    a dedicated job which uses the checkpoints saved during the training
    (side-car evaluation).
    
    The model is trained on CIFAR10 dataset.

---
+++ distribution_strategy/parameter-server-training/README.md
@@ -0,0 +1,172 @@
+# Parameter Server Training Using Distribution Strategies
+
+This directory provides an example of running parameter server training with
+Distribution Strategies.
+
+Please first read the
+[documentation](https://www.tensorflow.org/tutorials/distribute/parameter_server_training)
+of Distribution Strategy for parameter server training. We also assume that readers
+of this page  are familiar with [Google Cloud](https://cloud.google.com/) and
+its [Kubernetes Engine](https://cloud.google.com/kubernetes-engine/).
+
+This directory contains the following files:
+
+-   template.yaml.jinja: jinja template used for generating Kubernetes manifests
+-   render_template.py: script for rendering the jinja template
+-   resnet_cifar_ps_strategy.py: script for running any type of parameter server training task based on `TF_CONFIG` environment variable
+-   Dockerfile.resnet_cifar_ps_strategy: a docker file to build the model image
+
+## Prerequisites
+
+1.  First you need to have a Google Cloud project. Either create a new project or use an existing one. 
+
+2.  Install
+    [gcloud commandline tools](https://cloud.google.com/functions/docs/quickstart)
+    on your system, login, set project and zone, etc.
+
+3.  Install [Docker](https://docs.docker.com/get-docker/) for your system 
+
+4.  Install kubectl:
+
+    ```bash
+    gcloud components install kubectl
+    ```
+5.  Start a Kubernetes cluster either with `gcloud` command as shown below or with
+    [GKE](https://cloud.google.com/kubernetes-engine/) web UI. Using more CPUs or nodes may require increasing your CPU [quotas](https://cloud.google.com/compute/quotas#requesting_additional_quota). 
+    
+    ```bash
+    gcloud container clusters create <cluster_name> --zone=us-west1-a --num-nodes=6 --machine-type=e2-standard-4 
+    ```
+
+6.  Set context for `kubectl` so that `kubectl` knows which cluster to use:
+
+    ```bash
+    kubectl config use-context <cluster_name>
+    ```
+
+7. Create a
+    [service account](https://cloud.google.com/compute/docs/access/service-accounts) 
+    and download its key file in JSON format. Assign Storage Admin role for 
+    [Google Cloud Storage](https://cloud.google.com/storage/) to this service account:
+
+    ```bash
+    gcloud iam service-accounts create <service_account_id> --display-name="<display_name>"
+    ```
+
+    ```bash
+    gcloud projects add-iam-policy-binding <project-id> \
+    --member="serviceAccount:<service_account_id>@<project_id>.iam.gserviceaccount.com" \
+    --role="roles/storage.admin"
+    ```
+
+8.  Create a Kubernetes secret from the JSON key file of your service account:
+
+    ```bash
+    kubectl create secret generic credential --from-file=key.json=<path_to_json_file>
+    ```
+
+9. Enable GCR ([Google Container Registry](https://cloud.google.com/container-registry)) service for your project using either GCP web UI or gcloud tool:
+
+    ```bash
+    gcloud services enable containerregistry.googleapis.com
+    ```
+
+10. Configure Docker to authenticate with Container Registry
+
+    ```bash
+    gcloud auth configure-docker
+    ```
+## How to run the example
+
+1.  Create three buckets for model data, checkpoints and training logs using either GCP web UI or gsutil tool (included with the gcloud tool you have installed above):
+
+    ```bash
+    gsutil mb gs://<bucket_name>
+    ```
+    You will use these bucket names to modify `data_dir`, `checkpoint_dir` and `train_log_dir` in step #4.
+
+
+2. Download CIFAR-10 data and place them in your data_dir bucket. Head to the [ResNet in TensorFlow](https://github.com/tensorflow/models/tree/r1.13.0/official/resnet#cifar-10) directory to obtain CIFAR-10 data. Alternatively, you can use this [direct link](https://www.cs.toronto.edu/~kriz/cifar-10-binary.tar.gz) to download and extract the data yourself as well. 
+    
+    ```bash
+    python cifar10_download_and_extract.py
+    ```
+
+    Upload the contents of cifar-10-batches-bin directory to your `data_dir` bucket.
+
+    ```bash
+    gsutil -m cp cifar-10-batches-bin/* gs://<your_data_dir>/
+
+    ```
+
+3.  Now let's build the Docker image:
+
+    ```bash
+    docker build --no-cache -t resnet_cifar_ps_strategy:v1 -f Dockerfile.resnet_cifar_ps_strategy .
+
+    ```
+
+    and push the image to
+    [Google Cloud Container Registery](https://cloud.google.com/container-registry/):
+
+    ```bash
+    docker tag resnet_cifar_ps_strategy:v1 gcr.io/<your project>/resnet_cifar_ps_strategy:v1
+    docker push gcr.io/<your project>/resnet_cifar_ps_strategy:v1
+    ```
+
+4.  Modify the variables in template.yaml.jinja. You may want to change `name`,
+    `image`, `train_log_dir`, `script` and `cmdline_args`.
+
+    *   `name`: name your cluster, e.g. "my-parameter-server-example".
+    *   `image`: the name of your docker image.
+    *   `worker_replicas`: number of worker pods. 
+    *   `ps_replicas`: number of parameter server pods.
+    *   `num_gpus_per_worker`: number of GPUs (this does not apply for this example since parameter server distribution strategy does not have GPU support yet) 
+    *   `has_coordinator`: flag for creating coordinator job
+    *   `has_chief`: flag for creating chief job
+    *   `has_tensorboard`: flag for creating tensorboard job
+    *   `script`: the script in the docker image to run.
+    *   `train_log_dir`: used for logging training accuracy
+    *   `cmdline_args`: the command line arguments passed to the `script`.
+    *   `credential_secret_json`: the filename that was registered to Kubernetes as a secret. 
+    *   `credential_secret_key`: the name of the Kubernetes secret used for storing
+        your service account key.
+    *   `port`: the port for all tasks including tensorboard.
+    *   `use_node_port`: flag for using NodePort as type of service. Jinja template generates ingress only for tensorboard when this flag is set to `true`. Setting this flag to `false` enables LoadBalancer for all pods; assigning them external IPs (which may be limited by your public IP address quota).  
+
+5.  Start the training and evaluation on the cluster.
+    
+    You may want to verify the generated kubernetes manifestsby running the following:
+
+    ```bash
+    python render_template.py template.yaml.jinja | kubectl create -f - --dry-run
+    ```
+
+    After making sure that the above command succeeds, you can start the cluster (removing the dry-run flag):
+
+    ```bash
+    python render_template.py template.yaml.jinja | kubectl create -f -
+    ```
+    You'll see that your cluster has started training. You can inspect logs of
+    workers or use tensorboard to watch your model training.
+
+    ```bash
+    kubectl get pods
+    ```
+
+    ```bash
+    kubectl logs -f <pod_id>
+    ```
+
+6.  You can find the TensorBoard service public IP address on Services & Ingress page of GKE, and access TensorBoard on  http://<tensorboard_ip> (or http://<tensorboard_ip>:5000 if you have set `use_node_port` to `false`)using your browser.  
+    
+    The training accuracy graph shall look like the following:
+
+    ![Traning accuracy - Tensorboard](images/tf-dist-ps-tensorboard.png)
+
+7. Destroy the cluster
+    
+    ```bash
+    gcloud container clusters delete <cluster_name>
+    ```
+
---
+++ distribution_strategy/parameter-server-training/resnet_cifar_ps_strategy.py
@@ -0,0 +1,240 @@
+# Copyright 2015 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# =============================================================================
+
+# Lint as: python3
+"""ResNet Cifar + ParameterServerStrategy example.
+"""
+import os
+from datetime import datetime
+from absl import app
+from absl import flags
+from absl import logging
+import portpicker
+import tensorflow as tf
+from tensorflow.python.distribute import parameter_server_strategy_v2
+from tensorflow_models.official.benchmark.models import cifar_preprocessing
+from tensorflow_models.official.benchmark.models import resnet_cifar_model
+from tensorflow_models.official.vision.image_classification.resnet import common as img_class_common
+
+flags.DEFINE_string("checkpoint_dir", "gs://cifar10_ckpt/",
+                    "Directory for writing model checkpoints.")
+flags.DEFINE_string("data_dir", "gs://cifar10_data/",
+                    "Directory for Resnet Cifar model input. Follow the "
+                    "instruction here to get Cifar10 data: "
+                    "https://github.com/tensorflow/models/tree/r1.13.0/official/resnet#cifar-10")
+flags.DEFINE_string("train_log_dir", "gs://cifar10_train_log/",
+                    "Directory for Resnet Cifar training logs")
+flags.DEFINE_boolean(
+    "use_in_process_cluster", False,
+    "Whether to use in-process cluster for testing.")
+flags.DEFINE_boolean(
+    "run_in_process_training", True,
+    "Whether to use in-process cluster to run training or evaluation.")
+
+FLAGS = flags.FLAGS
+
+TRAIN_EPOCHS = 182
+STEPS_PER_EPOCH = 781
+BATCH_SIZE = 64
+
+
+def create_in_process_cluster(num_workers, num_ps):
+  """Creates and starts local servers and returns the cluster_spec dict."""
+  worker_ports = [portpicker.pick_unused_port() for _ in range(num_workers)]
+  ps_ports = [portpicker.pick_unused_port() for _ in range(num_ps)]
+
+  cluster_dict = {}
+  cluster_dict["worker"] = ["localhost:%s" % port for port in worker_ports]
+  if num_ps > 0:
+    cluster_dict["ps"] = ["localhost:%s" % port for port in ps_ports]
+
+  cluster_spec = tf.train.ClusterSpec(cluster_dict)
+
+  for i in range(num_workers):
+    tf.distribute.Server(
+        cluster_spec,
+        job_name="worker",
+        protocol="grpc",
+        task_index=i,
+        start=True)
+
+  for i in range(num_ps):
+    tf.distribute.Server(
+        cluster_spec,
+        job_name="ps",
+        protocol="grpc",
+        task_index=i,
+        start=True)
+
+  cluster_resolver = tf.distribute.cluster_resolver.SimpleClusterResolver(
+      cluster_spec, rpc_layer="grpc")
+  return cluster_resolver
+
+def run_tf_server_and_wait(cluster_resolver):
+  assert cluster_resolver.task_type in ("worker", "ps")
+  server = tf.distribute.Server(
+    cluster_resolver.cluster_spec(),
+    job_name=cluster_resolver.task_type,
+    task_index=cluster_resolver.task_id,
+    protocol=cluster_resolver.rpc_layer or "grpc",
+    start=True)
+  server.join()
+
+
+def train_resnet_cifar(cluster_resolver):
+  """Trains the resnet56 model using parameter server distribution strategy.
+
+  Args:
+      cluster_resolver: cluster resolver to give neccessary information to
+        set up distributed training
+  """
+
+  strategy = parameter_server_strategy_v2.ParameterServerStrategyV2(
+      cluster_resolver)
+  coordinator = (
+        tf.distribute.experimental.coordinator.ClusterCoordinator(strategy))
+  with strategy.scope():
+    model = resnet_cifar_model.resnet56()
+
+    initial_learning_rate = (
+        img_class_common.BASE_LEARNING_RATE * BATCH_SIZE / 128)
+    # Using the learning rate schedule from the model garden:
+    # tensorflow_models/official/benchmark/models/resnet_cifar_main.py
+    lr_segments = [  # (multiplier, epoch to start) tuples
+        (0.1, 91), (0.01, 136), (0.001, 182)
+    ]
+    lr_schedule = tf.keras.optimizers.schedules.PiecewiseConstantDecay(
+        boundaries=list(p[1] * STEPS_PER_EPOCH for p in lr_segments),
+        values=[initial_learning_rate] +
+        list(p[0] * initial_learning_rate for p in lr_segments))
+    optimizer = img_class_common.get_optimizer(lr_schedule)
+
+    train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(
+        name="train_accuracy")
+
+    @tf.function
+    def worker_train_fn(iterator):
+
+      def replica_fn(inputs):
+        """Training loop function."""
+        batch_data, labels = inputs
+        with tf.GradientTape() as tape:
+          predictions = model(batch_data, training=True)
+          xent_loss = tf.keras.losses.SparseCategoricalCrossentropy(
+              reduction=tf.keras.losses.Reduction.NONE)(labels, predictions)
+          loss = (
+              tf.nn.compute_average_loss(xent_loss) +
+              tf.nn.scale_regularization_loss(model.losses))
+        gradients = tape.gradient(loss, model.trainable_variables)
+
+        optimizer.apply_gradients(
+            zip(gradients, model.trainable_variables))
+        train_accuracy.update_state(labels, predictions)
+
+      inputs = next(iterator)
+      strategy.run(replica_fn, args=(inputs,))
+
+    checkpoint_manager = tf.train.CheckpointManager(
+        tf.train.Checkpoint(model=model, optimizer=optimizer),
+        FLAGS.checkpoint_dir,
+        max_to_keep=2)
+    if checkpoint_manager.latest_checkpoint:
+      checkpoint = checkpoint_manager.checkpoint
+      checkpoint.restore(
+          checkpoint_manager.latest_checkpoint
+          ).assert_existing_objects_matched()
+
+    dataset_fn = lambda: cifar_preprocessing.input_fn(
+        is_training=True,
+        data_dir=FLAGS.data_dir,
+        batch_size=BATCH_SIZE,
+        parse_record_fn=cifar_preprocessing.parse_record,
+        dtype=tf.float32,
+        drop_remainder=True)
+    per_worker_dataset = coordinator.create_per_worker_dataset(dataset_fn)
+
+  global_steps = int(optimizer.iterations.numpy())
+  logging.info("Training starts with global_steps = %d", global_steps)
+  current_time = datetime.now().strftime("%Y%m%d-%H%M%S")
+  train_log_dir = FLAGS.train_log_dir + current_time
+  train_summary_writer = tf.summary.create_file_writer(train_log_dir)
+
+  for epoch in range(global_steps // STEPS_PER_EPOCH,
+                     TRAIN_EPOCHS):
+    iterator = iter(per_worker_dataset)
+    for _ in range(STEPS_PER_EPOCH):
+      coordinator.schedule(worker_train_fn, args=(iterator,))
+    coordinator.join()
+    logging.info("Finished joining at epoch %d. Training accuracy: %f.",
+                  epoch, train_accuracy.result())
+    with train_summary_writer.as_default():
+      tf.summary.scalar('accuracy', train_accuracy.result(), step=epoch)
+    train_accuracy.reset_states()
+    checkpoint_manager.save()
+
+
+def evaluate_resnet_cifar():
+  """Evaluates the resnet56 model
+
+  This method provides side-car evaluation using the checkpoints
+
+  """
+  eval_model = resnet_cifar_model.resnet56()
+  eval_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(
+        name="eval_accuracy")
+  eval_model.compile(metrics=eval_accuracy)
+  eval_dataset = cifar_preprocessing.input_fn(
+      is_training=False,
+      data_dir=FLAGS.data_dir,
+      batch_size=BATCH_SIZE,
+      parse_record_fn=cifar_preprocessing.parse_record)
+  checkpoint = tf.train.Checkpoint(model=eval_model)
+
+  for latest_checkpoint in tf.train.checkpoints_iterator(
+      FLAGS.checkpoint_dir):
+    try:
+      checkpoint.restore(latest_checkpoint).expect_partial()
+    except tf.errors.OpError:
+      # checkpoint may be deleted by training when it is about to read it.
+      continue
+
+    # Optionally add callbacks to write summaries.
+    eval_model.evaluate(eval_dataset)
+
+    # Evaluation finishes when it has evaluated the last epoch.
+    if latest_checkpoint.endswith("-{}".format(TRAIN_EPOCHS)):
+      break
+
+
+def main(_):
+  if FLAGS.use_in_process_cluster:
+    if FLAGS.run_in_process_training:
+      cluster_resolver = create_in_process_cluster(3, 1)
+      train_resnet_cifar(cluster_resolver)
+    else:
+      evaluate_resnet_cifar()
+  else:
+    os.environ["grpc_fail_fast"] = "use_caller"
+    cluster_resolver = tf.distribute.cluster_resolver.TFConfigClusterResolver()
+    if cluster_resolver.task_type in ("worker", "ps"):
+      run_tf_server_and_wait(cluster_resolver)
+    elif cluster_resolver.task_type == "evaluator":
+      evaluate_resnet_cifar()
+    else:
+      train_resnet_cifar(cluster_resolver)
+
+
+if __name__ == "__main__":
+  app.run(main)
